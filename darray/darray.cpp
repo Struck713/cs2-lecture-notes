b165056361fa5f660781e04ec5a050202d2ce62c
Struck713 <noahstruck71303@gmail.com>
2022-02-23 13:27:30 -0500

    Updated darray.cpp and darray.hpp

--- darray/darray.cpp
+++ darray/darray.cpp
@@ -1,5 +1,9 @@
+//////////////////////////////////////
+//SIMPLE DYNAMICALLY ALLOCATED ARRAY//
+//////////////////////////////////////
 
-#include "darray.hpp"
+
+#include "darray.h"
 #include <cstdlib>
 
 Darray::Darray() {
@@ -7,14 +11,80 @@ Darray::Darray() {
     ptr = 0;
 }
 
-// REQUIRES: 0 <= n
+// REQUIRES 0 <= n
 Darray::Darray(int n) {
     if (n < 0) n = 0;
-
     cap = n;
     ptr = new int[cap];
 }
 
+// Can NOT call exit() - no assert()
+// Would infinitely call eachother
 Darray::~Darray() {
     delete [] ptr;
+}
+
+// Must be passed by reference or would result in infinite loop
+Darray::Darray(const Darray& actual) {  // copy ctor
+    cap = actual.cap;
+    ptr = new int[cap];
+
+    for (int i = 0; i < cap; ++i) {
+        ptr[i] = actual.ptr[i];
+    }
+}
+
+// Similar to the copy constructer but we had to do some simple cleanup.
+// Old fashioned way - sortof
+
+/*Darray& Darray::operator= (const Darray& rhs) {
+    if (ptr != rhs.ptr) {
+        delete [] ptr;
+        cap = rhs.cap;
+        ptr = new int[cap];
+        for (int i = 0; i < cap; ++i) {
+            ptr[i] = rhs.ptr[i];
+        }
+    }
+    return *this;
+}
+*/
+
+// New way, one or the other.
+Darray& Darray::operator= (Darray rhs) {
+    swap(rhs); 
+    return *this;
+}
+
+// Constant time swap - no matter how big the array is.
+void Darray::swap(Darray& rhs) {
+    int* temp = ptr;
+    ptr = rhs.ptr;
+    rhs.ptr = temp;
+
+    int tcap = cap;
+    cap = rhs.cap;
+    rhs.cap = tcap;
+}
+
+int Darray::operator[](int i) const {
+    assert(i >= 0 && i < cap);
+    return ptr[i];
+}
+
+int& Darray::operator[](int i) {
+    assert(i >= 0 && i < cap);
+    return ptr[i];
+}
+
+void Darray::resize(int newCap) {
+    int smaller = newCap;
+    if (cap < newCap) smaller = cap;
+
+    int* temp = new int[newCap];
+    for (int i = 0; i < smaller; ++i)
+        temp[i] = ptr[i];
+
+    delete [] ptr;
+    ptr = temp;
 }
--- darray/darray.hpp
+++ darray/darray.hpp
@@ -1,29 +1,37 @@
 #ifndef CS2_ARRAY_H_
 #define CS2_ARRAY_H_
 
+
+/////////////////////////////////////
+//Simple Dynamically Allocated Array
+/////////////////////////////////////
+
 #include <iostream>
 #include <cassert>
 
+//
+// CLASS INV: ptr -> int[cap]
+//
+
 class Darray {
 public:
-    Darray();
-    Darray(int);
-    Darray(const Darray&); // copy ctor
-    ~Darray();             // destructor
-    
-    void swap(Darray&);
-    Darray& operator=(const Darray&) const;
-    Darray& operator=(Darray) const;
+            Darray();
+            Darray(int);
+            Darray(const Darray&); // Copy Constructor
+            ~Darray();             // Destructor
+    void    swap(Darray&);
 
-    int operator[](int) const;
-    int& operator[](int);
-    int capacity() const;
-    void resize(int);
+    //Darray& operator=(const Darray&);
+    Darray& operator=(Darray);
+    int operator   [](int) const;
+    int& operator  [](int);
+
+    int capacity() const { return cap; }
+    void resize (int);
 
 private:
-    int* ptr;
+    int *ptr;
     int cap;
-
-}
+};
 
 #endif
